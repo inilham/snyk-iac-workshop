94cce700058be7244a3629ce5de7d4036d3e28bc
inilham <129713142+inilham@users.noreply.github.com>
2023-05-11 09:02:01 +0700

    Update big_data.tf

--- terraform/big_data.tf
+++ terraform/big_data.tf
@@ -11,7 +11,7 @@ resource google_sql_database_instance "master_instance" {
         name  = "WWW"
         value = "0.0.0.0/0"
       }
-      require_ssl = false
+      require_ssl = true
     }
     backup_configuration {
       enabled = false
